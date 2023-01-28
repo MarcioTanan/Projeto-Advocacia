0fd4bccf67228e1f67bdac50a469c5a0772a9477
Marcio Tanan <marcio-tanan@hotmail.com>
2023-01-28 18:27:34 -0300

    update js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// SEleção de elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+
+//Funções
+
+
+
+//Eventos
+[menuBtn, closeMenuBtn].forEach((btn) =>{
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active",)
+
+    })
+
+})
