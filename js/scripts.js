0a71958dc6d2ddc323081b81a54e5494d93146a1
Marcio Tanan <marcio-tanan@hotmail.com>
2023-01-28 18:52:26 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
-// SEleção de elementos
+// Seleção de elementos
 const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a")
+const mobileLinks = document.querySelectorAll("#mobile-navbar a")
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+
+
+
+
 
 //Funções
+function smoothScroll(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
 
+    scroll ({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+
+      if(menu.classList.contains("menu-active")) {
+          menu.classList.remove("menu-active");
+      }
+    }, 500)
+}
 
 
 //Eventos
@@ -14,5 +38,11 @@ const menu = document.querySelector("#mobile-navbar");
         menu.classList.toggle("menu-active",)
 
     })
+
+})
+
+allLinks.forEach((link) => {
+
+  link.addEventListener("click", smoothScroll)
 
 })
